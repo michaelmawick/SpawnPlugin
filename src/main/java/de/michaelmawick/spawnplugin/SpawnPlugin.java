6b493939418d0ff7f494d79db1ccc92307eda7dc
Michael <michaeljens09@gmail.com>
2025-10-21 21:58:33 +0200

    first commit

---
+++ src/main/java/de/michaelmawick/spawnplugin/SpawnPlugin.java
@@ -0,0 +1,16 @@
+package de.michaelmawick.spawnplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
